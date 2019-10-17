3b20c7887c810db1f7487be57b8fda5d22a34fbb
Mizanur Rahman <35344192+Mizanur888@users.noreply.github.com>
2019-10-16 21:05:55 -0400

    Fixed bug for the Api results

--- src/app/components/leaderboard/leaderboard.component.ts
+++ src/app/components/leaderboard/leaderboard.component.ts
@@ -5,6 +5,7 @@ import { Observable } from 'rxjs';
 import { throwError } from 'rxjs';
 import { retry, catchError } from 'rxjs/operators';
 import { ServiceService } from 'src/app/Service/service.service';
+import { WebApiCallService } from 'src/app/Service/webApiCall.service';
 @Component({
   selector: 'app-leaderboard',
   templateUrl: './leaderboard.component.html',
@@ -18,6 +19,7 @@ export class LeaderboardComponent implements OnInit {
   steps: number[];
   rank: number[];
   testData: any[] = [];
+  testData2: any[] = [];
   get searchTerm(): string {
     return this._searchTerm;
   }
@@ -36,7 +38,7 @@ export class LeaderboardComponent implements OnInit {
       title.name.first.toLowerCase().indexOf(titleProject.toLowerCase()) >= 0);
 
   }
-  constructor(private http: HttpClient, private service: ServiceService) { }
+  constructor(private http: HttpClient, private service: ServiceService, private webApiService: WebApiCallService) { }
 
   ngOnInit() {
     // this.dataService.sendGetRequest().subscribe((data: any[]) => {
@@ -44,23 +46,53 @@ export class LeaderboardComponent implements OnInit {
     //   this.products = data;
 
     // })
+    this.getDataFromWebApiCall();
     this.rank = this.getRank();
     this.steps = this.getRandomNumber().sort((a, b) => 0 - (a > b ? 1 : -1));
     console.log(this.steps);
-    this.getData();
+    this.getDatatest();
   }
 
-  getData() {
+  // getData() {
 
-    return this.service.sendGetRequest().subscribe(response => {
+  //   return this.service.sendGetRequest().subscribe(response => {
 
-      console.log("users", response);
-      this.testData = response.results;
-      this.users = this.testData;
-      console.log(this.users)
+  //     console.log("users", response);
+  //     this.testData = response.results;
+  //     this.users = this.testData;
+  //     console.log(this.users)
+  //   }).unsubscribe();
+  // }
+
+  getDatatest() {
+    this.service.sendGetRequest().toPromise().then(promise => {
+      let data: any[] = [];
+      data.push(promise);
+      data.forEach(element => {
+
+        for (var i = 0; i < element.results.length; i++) {
+          this.testData.push(element.results)
+          console.log(element.results[i])
+        }
+      });
     })
   }
 
+  getDataFromWebApiCall() {
+    this.webApiService.dataFromWebApi().toPromise().then(data => {
+      console.log(data);
+      let temp: any[] = [];
+      temp.push(data);
+      temp.forEach(element => {
+        for (var i = 0; i < element.length; i++) {
+          console.log(element[i].FName)
+          this.testData2.push(element[i]);
+        }
+      });
+    }).catch(error => {
+      console.log(error)
+    })
+  }
   getRandomNumber() {
     let aa = new Array();
     for (var i = 0; i < 10; i++) {
